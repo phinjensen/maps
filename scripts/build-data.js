3b9ac84f23357a2ed9fa610efc0a0465837c8126
Phineas Jensen <phin@zayda.net>
2024-02-15 08:45:43 -0700

    Add Mexican States to build script

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -51,6 +62,7 @@ var shapefile_1 = require("shapefile");
 var yauzl_1 = require("yauzl");
 var ADVISORY_URL = "https://cadatacatalog.state.gov/dataset/4a387c35-29cb-4902-b91d-3da0dc02e4b2/resource/4c727464-8e6f-4536-b0a5-0a343dc6c7ff/download/traveladvisory.xml";
 var NATURAL_EARTH_URL = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip";
+var MEXICAN_STATES_URL = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip";
 var LOOKUPS = {
     "Burma (Myanmar)": "Myanmar",
     "Eswatini": "eSwatini",
@@ -63,7 +75,29 @@ var LOOKUPS = {
     "Timor-Leste": "East Timor",
     "Micronesia": "Federated States of Micronesia",
     "Sao Tome and Principe": "São Tomé and Principe",
+    "See State Summaries": "Mexico",
 };
+var MEXICO_LOOKUPS = {
+    "Nuevo Leon": "Nuevo León",
+    "San Luis Potosi": "San Luis Potosí",
+    "Michoacan": "Michoacán",
+    "Queretaro": "Querétaro",
+    "Yucatan": "Yucatán",
+    "Mexico City": "Distrito Federal",
+    "Mexico State": "México",
+};
+var LEVEL_TEXT_TO_NUMBER = {
+    "Exercise Normal Precautions": 1,
+    "Exercise Increased Caution": 2,
+    "Reconsider Travel": 3,
+    "Do Not Travel": 4
+};
+function fix_null_string(s) {
+    return s.replace(/\0/g, '').trim();
+}
+/**
+ * Fetch and parse data from the State Department
+ */
 function getStateDepartmentData() {
     return __awaiter(this, void 0, void 0, function () {
         var advisoryResponse, advisoryXml, parser, advisories;
@@ -82,71 +116,120 @@ function getStateDepartmentData() {
         });
     });
 }
-function getNaturalEarthData() {
+function readShapefileZip(buffer) {
     return __awaiter(this, void 0, void 0, function () {
-        var naturalEarthResponse, naturalEarthBuffer;
-        var _this = this;
         return __generator(this, function (_a) {
             switch (_a.label) {
+                case 0: return [4 /*yield*/, new Promise(function (resolve) {
+                        // unzip file being downloaded
+                        (0, yauzl_1.fromBuffer)(Buffer.from(buffer), { lazyEntries: true }, function (err, zipfile) {
+                            if (err)
+                                throw err;
+                            var shpStream, dbfStream;
+                            // read entries, saving the .shp and .dbf files as streams
+                            zipfile.readEntry();
+                            zipfile.on("entry", function (entry) {
+                                if (entry.fileName.endsWith(".shp")) {
+                                    zipfile.openReadStream(entry, function (err, readStream) {
+                                        if (err)
+                                            throw err;
+                                        shpStream = readStream;
+                                    });
+                                }
+                                else if (entry.fileName.endsWith(".dbf")) {
+                                    zipfile.openReadStream(entry, function (err, readStream) {
+                                        if (err)
+                                            throw err;
+                                        dbfStream = readStream;
+                                    });
+                                }
+                                zipfile.readEntry();
+                            });
+                            // once done reading the zip file, load the shapefile and organize by name (using multiple names to be thorough)
+                            zipfile.on("end", function () { return (0, shapefile_1.read)(shpStream, dbfStream, { encoding: "UTF-8" }).then(function (source) { return resolve(source); }); });
+                        });
+                    })];
+                case 1: return [2 /*return*/, _a.sent()];
+            }
+        });
+    });
+}
+/** Fetch, parse, and organize sovereign countries from https://www.naturalearthdata.com/ by name */
+function getNaturalEarthData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var naturalEarthResponse, naturalEarthBuffer, collection, _i, _a, feature;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
                 case 0: return [4 /*yield*/, fetch(NATURAL_EARTH_URL)];
+                case 1:
+                    naturalEarthResponse = _b.sent();
+                    return [4 /*yield*/, naturalEarthResponse.arrayBuffer()];
+                case 2:
+                    naturalEarthBuffer = _b.sent();
+                    return [4 /*yield*/, readShapefileZip(naturalEarthBuffer)];
+                case 3:
+                    collection = _b.sent();
+                    for (_i = 0, _a = collection.features; _i < _a.length; _i++) {
+                        feature = _a[_i];
+                        if (feature.properties) {
+                            feature.properties = {
+                                names: fix_null_string(feature.properties.NAME),
+                                geounit: fix_null_string(feature.properties.GEOUNIT),
+                                sovereign: fix_null_string(feature.properties.SOVEREIGNT)
+                            };
+                        }
+                    }
+                    return [2 /*return*/, collection.features.reduce(function (obj, cur) {
+                            var _a, _b, _c;
+                            // Create lookup dictionaries based on name, geounit, and sovereign name properties of the data
+                            var name = (_a = cur.properties) === null || _a === void 0 ? void 0 : _a.name;
+                            var geounit = (_b = cur.properties) === null || _b === void 0 ? void 0 : _b.geounit;
+                            var sovereign = (_c = cur.properties) === null || _c === void 0 ? void 0 : _c.sovereign;
+                            obj.name[name] = __spreadArray(__spreadArray([], (obj.name[name] || []), true), [cur], false);
+                            obj.geounit[geounit] = __spreadArray(__spreadArray([], (obj.geounit[geounit] || []), true), [cur], false);
+                            obj.sovereign[sovereign] = __spreadArray(__spreadArray([], (obj.sovereign[sovereign] || []), true), [cur], false);
+                            return obj;
+                        }, { name: {}, geounit: {}, sovereign: {} })];
+            }
+        });
+    });
+}
+/** Fetch, parse, and organize sovereign countries from https://www.naturalearthdata.com/ by name */
+function getMexicanStates() {
+    return __awaiter(this, void 0, void 0, function () {
+        var naturalEarthResponse, naturalEarthBuffer, collection, result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch(MEXICAN_STATES_URL)];
                 case 1:
                     naturalEarthResponse = _a.sent();
                     return [4 /*yield*/, naturalEarthResponse.arrayBuffer()];
                 case 2:
                     naturalEarthBuffer = _a.sent();
-                    return [4 /*yield*/, new Promise(function (resolve) {
-                            (0, yauzl_1.fromBuffer)(Buffer.from(naturalEarthBuffer), { lazyEntries: true }, function (err, zipfile) {
-                                if (err)
-                                    throw err;
-                                var shpStream, dbfStream;
-                                zipfile.readEntry();
-                                zipfile.on("entry", function (entry) {
-                                    if (entry.fileName.endsWith(".shp")) {
-                                        zipfile.openReadStream(entry, function (err, readStream) {
-                                            if (err)
-                                                throw err;
-                                            shpStream = readStream;
-                                        });
-                                    }
-                                    else if (entry.fileName.endsWith(".dbf")) {
-                                        zipfile.openReadStream(entry, function (err, readStream) {
-                                            if (err)
-                                                throw err;
-                                            dbfStream = readStream;
-                                        });
-                                    }
-                                    zipfile.readEntry();
-                                });
-                                zipfile.on("end", function () {
-                                    (0, shapefile_1.read)(shpStream, dbfStream, { encoding: "UTF-8" }).then(function (source) { return __awaiter(_this, void 0, void 0, function () {
-                                        var _i, _a, feature;
-                                        return __generator(this, function (_b) {
-                                            for (_i = 0, _a = source.features; _i < _a.length; _i++) {
-                                                feature = _a[_i];
-                                                if (feature.properties) {
-                                                    feature.properties = {
-                                                        names: feature.properties.NAME.replace(/\0/g, '').trim(),
-                                                        geounit: feature.properties.GEOUNIT.replace(/\0/g, '').trim(),
-                                                        sovereign: feature.properties.SOVEREIGNT.replace(/\0/g, '').trim()
-                                                    };
-                                                }
-                                            }
-                                            resolve(source.features);
-                                            return [2 /*return*/];
-                                        });
-                                    }); });
-                                });
-                            });
-                        })];
-                case 3: return [2 /*return*/, _a.sent()];
+                    return [4 /*yield*/, readShapefileZip(naturalEarthBuffer)];
+                case 3:
+                    collection = _a.sent();
+                    result = collection.features.filter(function (feature) { return feature.properties.iso_a2 === 'MX'; }).map(function (feature) { return (__assign(__assign({}, feature), { properties: {
+                            name: fix_null_string(feature.properties.name),
+                        } })); });
+                    return [2 /*return*/, result.reduce(function (obj, cur) {
+                            var _a;
+                            var _b;
+                            return (__assign((_a = {}, _a[(_b = cur.properties) === null || _b === void 0 ? void 0 : _b.name] = cur, _a), obj));
+                        }, {})];
             }
         });
     });
 }
+// Transform the advisory data to have consistent names and the data we need
 function transformData(advisories) {
     return advisories.map(function (advisory) {
         var name, level;
-        if (advisory.title.startsWith("See Summaries - Mainland China")) {
+        if (advisory.title.startsWith("See State Summaries")) {
+            name = "Mexico";
+            level = advisory.title.split(" - ")[1];
+        }
+        else if (advisory.title.startsWith("See Summaries - Mainland China")) {
             name = "China";
             level = advisory.title.split(" - ")[2];
         }
@@ -170,23 +253,39 @@ function transformData(advisories) {
     });
 }
 Promise.all([
-    getNaturalEarthData().then(function (features) { return features.reduce(function (obj, cur) {
-        var name = cur.properties.name;
-        var geounit = cur.properties.geounit;
-        var sovereign = cur.properties.sovereign;
-        obj.name[name] = __spreadArray(__spreadArray([], (obj.name[name] || []), true), [cur], false);
-        obj.geounit[geounit] = __spreadArray(__spreadArray([], (obj.geounit[geounit] || []), true), [cur], false);
-        obj.sovereign[sovereign] = __spreadArray(__spreadArray([], (obj.sovereign[sovereign] || []), true), [cur], false);
-        return obj;
-    }, { name: {}, geounit: {}, sovereign: {} }); }),
+    getNaturalEarthData(),
+    getMexicanStates(),
     getStateDepartmentData()
         .then(function (advisories) { return transformData(advisories.feed.entry); })
 ]).then(function (_a) {
-    var geometry = _a[0], advisories = _a[1];
+    var geometry = _a[0], mexican_states = _a[1], advisories = _a[2];
     var features = [];
-    for (var _i = 0, advisories_1 = advisories; _i < advisories_1.length; _i++) {
-        var area = advisories_1[_i];
+    var _loop_1 = function (area) {
         var name_1 = (LOOKUPS[area.name] || area.name).trim();
+        if (name_1 === "Mexico") {
+            var stateSummarySection = area.summary.substring(area.summary.indexOf("<p><b><u>"));
+            var stateSummaries = stateSummarySection.replace(/\n/g, '').split("</p><p><b><u>");
+            stateSummaries = stateSummaries.map(function (summary) {
+                var name = summary.split(/( state| \([A-Za-z ]+\))(\s|&nbsp;)– /)[0].replace("<p><b><u>", "");
+                name = MEXICO_LOOKUPS[name] || name;
+                var match = mexican_states[name];
+                if (match) {
+                    match.properties = {
+                        name: name,
+                        level: LEVEL_TEXT_TO_NUMBER[summary.replace(/.*(\s|&nbsp;)– ([A-Za-z ]+)<\/u>.*/, "$2")],
+                        summary: summary.substring(summary.indexOf("</u></b></p>") + "</u></b></p>".length),
+                        link: area.link
+                    };
+                    features.push(match);
+                }
+                else {
+                    console.log("Couldn't find match for", name);
+                }
+            });
+            return "continue";
+        }
+        // Look based on name first, then geounit, then sovereign
+        // (matching only on sovereign can lead to e.g. tiny islands being chosen for Australia)
         var match = geometry.name[name_1] || geometry.geounit[name_1] || geometry.sovereign[name_1];
         if (match) {
             if (match.length > 1) {
@@ -199,6 +298,11 @@ Promise.all([
         else {
             console.error("couldn't find match for \"" + name_1 + "\"");
         }
+    };
+    // Iterate through advisories and find the matching geometry
+    for (var _i = 0, advisories_1 = advisories; _i < advisories_1.length; _i++) {
+        var area = advisories_1[_i];
+        _loop_1(area);
     }
     (0, fs_1.writeFileSync)('src/countries-with-advisories.json', JSON.stringify({ type: 'FeatureCollection', features: features }));
 });
--- scripts/build-data.ts
+++ scripts/build-data.ts
@@ -1,11 +1,13 @@
 import { XMLParser } from "fast-xml-parser";
 import { writeFileSync } from "fs";
-import { open as openShapefile, read as readShapefile } from "shapefile";
+import { FeatureCollection } from "geojson";
+import { Source, read as readShapefile } from "shapefile";
 import { fromBuffer as unzipBuffer } from "yauzl";
 
 const ADVISORY_URL =
   "https://cadatacatalog.state.gov/dataset/4a387c35-29cb-4902-b91d-3da0dc02e4b2/resource/4c727464-8e6f-4536-b0a5-0a343dc6c7ff/download/traveladvisory.xml";
 const NATURAL_EARTH_URL = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip";
+const MEXICAN_STATES_URL = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip"
 
 const LOOKUPS = {
   "Burma (Myanmar)": "Myanmar",
@@ -19,8 +21,33 @@ const LOOKUPS = {
   "Timor-Leste": "East Timor",
   "Micronesia": "Federated States of Micronesia",
   "Sao Tome and Principe": "São Tomé and Principe",
+  "See State Summaries": "Mexico",
 };
 
+const MEXICO_LOOKUPS = {
+  "Nuevo Leon": "Nuevo León",
+  "San Luis Potosi": "San Luis Potosí",
+  "Michoacan": "Michoacán",
+  "Queretaro": "Querétaro",
+  "Yucatan": "Yucatán",
+  "Mexico City": "Distrito Federal",
+  "Mexico State": "México",
+}
+
+const LEVEL_TEXT_TO_NUMBER = {
+  "Exercise Normal Precautions": 1,
+  "Exercise Increased Caution": 2,
+  "Reconsider Travel": 3,
+  "Do Not Travel": 4
+}
+
+function fix_null_string(s: string) {
+  return s.replace(/\0/g, '').trim();
+}
+
+/**
+ * Fetch and parse data from the State Department
+ */
 async function getStateDepartmentData() {
   const advisoryResponse = await fetch(ADVISORY_URL);
   const advisoryXml = await advisoryResponse.text();
@@ -30,13 +57,13 @@ async function getStateDepartmentData() {
   return advisories;
 }
 
-async function getNaturalEarthData() {
-  const naturalEarthResponse = await fetch(NATURAL_EARTH_URL);
-  const naturalEarthBuffer = await naturalEarthResponse.arrayBuffer();
+async function readShapefileZip(buffer: ArrayBuffer): Promise<FeatureCollection> {
   return await new Promise((resolve) => {
-    unzipBuffer(Buffer.from(naturalEarthBuffer), { lazyEntries: true }, (err, zipfile) => {
+    // unzip file being downloaded
+    unzipBuffer(Buffer.from(buffer), { lazyEntries: true }, (err, zipfile) => {
       if (err) throw err;
       let shpStream, dbfStream;
+      // read entries, saving the .shp and .dbf files as streams
       zipfile.readEntry();
       zipfile.on("entry", (entry) => {
         if (entry.fileName.endsWith(".shp")) {
@@ -52,29 +79,61 @@ async function getNaturalEarthData() {
         }
         zipfile.readEntry();
       });
-      zipfile.on("end", () => {
-        readShapefile(shpStream, dbfStream, { encoding: "UTF-8" }).then(async source => {
-          for (let feature of source.features) {
-            if (feature.properties) {
-              feature.properties = {
-                names: feature.properties.NAME.replace(/\0/g, '').trim(),
-                geounit: feature.properties.GEOUNIT.replace(/\0/g, '').trim(),
-                sovereign: feature.properties.SOVEREIGNT.replace(/\0/g, '').trim()
-              };
-            }
-          }
-          resolve(source.features);
-        }
-        );
-      });
-    })
-  });;
+
+      // once done reading the zip file, load the shapefile and organize by name (using multiple names to be thorough)
+      zipfile.on("end", () => readShapefile(shpStream, dbfStream, { encoding: "UTF-8" }).then(source => resolve(source)));
+    });
+  });
 }
 
+/** Fetch, parse, and organize sovereign countries from https://www.naturalearthdata.com/ by name */
+async function getNaturalEarthData() {
+  const naturalEarthResponse = await fetch(NATURAL_EARTH_URL);
+  const naturalEarthBuffer = await naturalEarthResponse.arrayBuffer();
+  const collection = await readShapefileZip(naturalEarthBuffer);
+  for (let feature of collection.features) {
+    if (feature.properties) {
+      feature.properties = {
+        names: fix_null_string(feature.properties.NAME),
+        geounit: fix_null_string(feature.properties.GEOUNIT),
+        sovereign: fix_null_string(feature.properties.SOVEREIGNT)
+      };
+    }
+  }
+  return collection.features.reduce((obj, cur) => {
+    // Create lookup dictionaries based on name, geounit, and sovereign name properties of the data
+    let name = cur.properties?.name;
+    let geounit = cur.properties?.geounit;
+    let sovereign = cur.properties?.sovereign;
+    obj.name[name] = [...(obj.name[name] || []), cur];
+    obj.geounit[geounit] = [...(obj.geounit[geounit] || []), cur];
+    obj.sovereign[sovereign] = [...(obj.sovereign[sovereign] || []), cur];
+    return obj;
+  }, { name: {}, geounit: {}, sovereign: {} });
+}
+
+/** Fetch, parse, and organize sovereign countries from https://www.naturalearthdata.com/ by name */
+async function getMexicanStates() {
+  const naturalEarthResponse = await fetch(MEXICAN_STATES_URL);
+  const naturalEarthBuffer = await naturalEarthResponse.arrayBuffer();
+  const collection = await readShapefileZip(naturalEarthBuffer);
+  const result = collection.features.filter(feature => feature.properties.iso_a2 === 'MX').map(feature => ({
+    ...feature,
+    properties: {
+      name: fix_null_string(feature.properties.name),
+    },
+  }));
+  return result.reduce((obj, cur) => ({ [cur.properties?.name]: cur, ...obj }), {});
+}
+
+// Transform the advisory data to have consistent names and the data we need
 function transformData(advisories: StateDepartmentAdvisory[]): Advisory[] {
   return advisories.map((advisory) => {
     let name, level;
-    if (advisory.title.startsWith("See Summaries - Mainland China")) {
+    if (advisory.title.startsWith("See State Summaries")) {
+      name = "Mexico";
+      level = advisory.title.split(" - ")[1];
+    } else if (advisory.title.startsWith("See Summaries - Mainland China")) {
       name = "China";
       level = advisory.title.split(" - ")[2];
     } else if (advisory.title.startsWith("See Individual Summaries")) {
@@ -97,21 +156,38 @@ function transformData(advisories: StateDepartmentAdvisory[]): Advisory[] {
 }
 
 Promise.all([
-  getNaturalEarthData().then((features: any) => features.reduce((obj, cur) => {
-    let name = cur.properties.name;
-    let geounit = cur.properties.geounit;
-    let sovereign = cur.properties.sovereign;
-    obj.name[name] = [...(obj.name[name] || []), cur];
-    obj.geounit[geounit] = [...(obj.geounit[geounit] || []), cur];
-    obj.sovereign[sovereign] = [...(obj.sovereign[sovereign] || []), cur];
-    return obj;
-  }, { name: {}, geounit: {}, sovereign: {} })),
+  getNaturalEarthData(),
+  getMexicanStates(),
   getStateDepartmentData()
     .then((advisories) => transformData(advisories.feed.entry))
-]).then(([geometry, advisories]: [any, any]) => {
+]).then(([geometry, mexican_states, advisories]: [any, any, any]) => {
   let features = [];
+  // Iterate through advisories and find the matching geometry
   for (let area of advisories) {
     let name = (LOOKUPS[area.name] || area.name).trim();
+    if (name === "Mexico") {
+      let stateSummarySection = area.summary.substring(area.summary.indexOf("<p><b><u>"));
+      let stateSummaries = stateSummarySection.replace(/\n/g, '').split("</p><p><b><u>");
+      stateSummaries = stateSummaries.map(summary => {
+        let name = summary.split(/( state| \([A-Za-z ]+\))(\s|&nbsp;)– /)[0].replace("<p><b><u>", "");
+        name = MEXICO_LOOKUPS[name] || name;
+        let match = mexican_states[name];
+        if (match) {
+          match.properties = {
+            name,
+            level: LEVEL_TEXT_TO_NUMBER[summary.replace(/.*(\s|&nbsp;)– ([A-Za-z ]+)<\/u>.*/, "$2")],
+            summary: summary.substring(summary.indexOf("</u></b></p>") + "</u></b></p>".length),
+            link: area.link
+          };
+          features.push(match);
+        } else {
+          console.log("Couldn't find match for", name);
+        }
+      });
+      continue;
+    }
+    // Look based on name first, then geounit, then sovereign
+    // (matching only on sovereign can lead to e.g. tiny islands being chosen for Australia)
     let match = geometry.name[name] || geometry.geounit[name] || geometry.sovereign[name];
     if (match) {
       if (match.length > 1) {
